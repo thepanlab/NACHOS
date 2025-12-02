6a55ee62842160651186af02e4926c558eac592b
Paul Calle <pcallec@ou.edu>
2025-12-01 18:29:47 -0600

    Add bash for randomized_label

---
+++ scripts/training/training_config_files/loop_inner/randomized_label/randomized_label.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=0 python -m training.training_sequential.loop_inner.training_inner_loop --file /home/pcallec/mif_outer/scripts/training/training_config_files/loop_inner/randomized_label/config_inner_InceptionV3.json
