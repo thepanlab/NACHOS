f08930f209abeec636ac9cb634870d9b2b41290f
Paul Calle <pcallec@ou.edu>
2025-12-02 00:17:29 -0600

    Add configurations for randomized trial

--- scripts/training/training_config_files/loop_inner/randomized_label/randomized_label.sh
+++ scripts/training/training_config_files/loop_inner/randomized_label/randomized_label.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0 python -m training.training_sequential.loop_inner.training_inner_loop --file /home/pcallec/mif_outer/scripts/training/training_config_files/loop_inner/randomized_label/config_inner_InceptionV3.json
+
+CUDA_VISIBLE_DEVICES=0 python -m training.training_sequential.loop_inner.training_inner_loop --file /home/pcallec/mif_outer/scripts/training/training_config_files/loop_inner/randomized_label/config_inner_InceptionV3_AILU486-left.json
+
+CUDA_VISIBLE_DEVICES=1 python -m training.training_sequential.loop_inner.training_inner_loop --file /home/pcallec/mif_outer/scripts/training/training_config_files/loop_inner/randomized_label/config_inner_InceptionV3_AJBH331-left.json
